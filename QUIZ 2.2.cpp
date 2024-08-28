23de393b25357a7685e0547942c43e16f54623e1
jheracadiz <jheadorna.08@gmail.com>
2024-08-28 18:06:23 +0700

    Add files via upload

---
+++ QUIZ 2.2.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int x = 10;
+    int counter = 8;
+
+    while (counter <= x) {
+        cout<< "value: " << counter<<endl;
+        counter++;     
+ }
+
+    return 0;
+}
